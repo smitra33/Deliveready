9c02e8a92ffaa642e1f3cd87217be833fec42aee
smitra33 <60757186+smitra33@users.noreply.github.com>
2022-03-26 16:31:58 -0600

    Added url for new apps

--- Deliveready/Deliveready/urls.py
+++ Deliveready/Deliveready/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('recipes/', include('recipes.urls')),
-    path('shoppingcart/', include('shoppingcart.urls'))
+    path('shoppingcart/', include('shoppingcart.urls')),
+    path('order/', include('orders.urls')),
+    path('pantry/', include('pantry.urls'))
 ]
