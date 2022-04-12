12a2aeea88a96b7113efacdd3cb555817240f9c0
smitra33 <smitra@ucalgary.ca>
2022-04-12 15:58:09 -0600

    Basic Javascript for Recipe template

--- Deliveready/recipes/static/js/recipe.js
+++ Deliveready/recipes/static/js/recipe.js
@@ -1,15 +1,31 @@
-let title = ''
-let instructions = ''
-let ingredients = {}
-let recipeInfo = {}
+let title = '';
+let instructions = '';
+let ingredients = [];
+let recipeInfo = '';
 
 async function getRecipeInfo(){
     // const response = await fetch('api/recipe_view/${id}/')
     const response = await fetch('http://127.0.0.1:8000/api/recipe_view/1/')
     recipeInfo = await response.json();
+    assignRecipeInfo();
 }
 
-async function displayRecipeInfo(){
-    getRecipeInfo();
+function assignRecipeInfo(){
+    title = recipeInfo.name;
+    recipeInfo['ingredients'].forEach(ing => {
+        ingredients.push(ing['name']);
+    });
+    instructions = recipeInfo.instructions;
+    displayElements();
 }
 
+function displayElements(){
+    document.getElementById('recipe-title').innerHTML = title;
+    var ingredientLength = ingredients.length;
+    var ingredientList = '';
+    for (var i = 0; i < ingredientLength; i++) {
+        ingredientList += `<li>${ingredients[i]}</li>`
+    }
+    document.getElementById('recipe-ingredients').innerHTML = ingredientList;
+    document.getElementById('recipe-instructions').innerHTML = instructions;
+}
