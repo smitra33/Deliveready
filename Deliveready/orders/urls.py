bf20f28fa2080218bf9299f0aa74da9cf4c870e2
dohkhanh <do.h.khanh@gmail.com>
2022-04-14 02:55:20 -0600

    payment screen goes to orders

--- Deliveready/orders/urls.py
+++ Deliveready/orders/urls.py
@@ -3,5 +3,6 @@ from . import views, api
 
 urlpatterns = [
     path('', views.order, name='order'),
-    path('view/<int:order_id>', api.OrderView.as_view(), name="order_view") 
+    path('view/<int:order_id>', api.OrderView.as_view(), name="order_view"), 
+    path('payment/', views.payment, name='payment')
 ]
--- Deliveready/orders/views.py
+++ Deliveready/orders/views.py
@@ -3,3 +3,6 @@ from .models import Order
 
 def order(request):
     return render(request, 'order.html')
+
+def payment(request):
+    return render(request,'payment.html')
