73e37ced4665a714a6241109d1fb7d9c420ad66b
smitra33 <smitra@ucalgary.ca>
2022-04-15 01:11:20 -0600

    CartIngredient API - Add Selected Ingredients to Cart

--- Deliveready/recipes/api.py
+++ Deliveready/recipes/api.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from .models import Recipe
 from ingredients.models import Ingredient
 from users.models import User
-from shoppingcart.models import ShoppingCart
+from shoppingcart.models import ShoppingCart, CartIngredient
 from pantry.models import Pantry
 
 
@@ -42,6 +42,23 @@ class RecipeView(APIView):
         return JsonResponse(data, safe=False)
 
 #add recipe ingredients to shopping cart
+# class AddRecipeIngredientsToCart(APIView):
+#     @action(detail=True, methods=['post'])
+#     def post(self, request, *args, **kwargs):
+#         try:
+#             recipe_id = request.data['recipe_id']
+#             recipe = Recipe.objects.filter(id=recipe_id)
+#             ingredient_list = recipe.values('ingredients')
+#             user = User.objects.filter(username=request.user).first()
+#             cart = ShoppingCart.objects.filter(user__id=user.id).first()
+#             for key in ingredient_list:
+#                 single_ing = Ingredient.objects.get(id=key['ingredients'])
+#                 cart.ingredients.add(single_ing)
+#             cart.save()
+#             return JsonResponse({'success': True, 'message': ''})
+#         except Exception as e:
+#             return JsonResponse({'success': False, 'message': str(e)})
+
 class AddRecipeIngredientsToCart(APIView):
     @action(detail=True, methods=['post'])
     def post(self, request, *args, **kwargs):
@@ -53,19 +70,26 @@ class AddRecipeIngredientsToCart(APIView):
             cart = ShoppingCart.objects.filter(user__id=user.id).first()
             for key in ingredient_list:
                 single_ing = Ingredient.objects.get(id=key['ingredients'])
-                cart.ingredients.add(single_ing)
-            cart.save()
+                CartIngredient.objects.create(ingredients=single_ing,shopping_cart=cart,quantity=single_ing.quantity)
             return JsonResponse({'success': True, 'message': ''})
         except Exception as e:
             return JsonResponse({'success': False, 'message': str(e)})
 
 #customer selection of ingredients after assessing duplicates added to shopping cart
-#input is a dictionary list of ingredients and quantity
+#input is a dictionary: ingredient name (key) and quantity (value) for example
 class AddSelectedIngredientsToCart(APIView):
     @action(detail=True, methods=['post'])
     def post(self, request, *args, **kwargs):
         try:
-
+            ing_dict = request.data
+            user = User.objects.filter(username=request.user).first()
+            cart = ShoppingCart.objects.filter(user__id=user.id).first()
+            for key, value in ing_dict.items():
+                single_ing = Ingredient.objects.get(name=key)
+                quantity = int(value)
+                product = CartIngredient.objects.filter(ingredients_id=single_ing.id).filter(shopping_cart_id=cart.id).first()
+                product.quantity = quantity
+                product.save()
             return JsonResponse({'success': True, 'message': ''})
         except Exception as e:
             return JsonResponse({'success': False, 'message': str(e)})
