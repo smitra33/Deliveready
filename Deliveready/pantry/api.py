e09c7620469198f67cb52a256d7aad80b91a0d49
Jon Cho <jonathan.cho@hotmail.com>
2022-04-14 23:25:23 -0600

    stuff
    stuff

---
+++ Deliveready/pantry/api.py
@@ -0,0 +1,39 @@
+from django.db.models import F
+from django.http import JsonResponse
+from django.views.generic import UpdateView
+from rest_framework.decorators import action
+from rest_framework.views import APIView
+from .models import Pantry
+from ingredients.models import Ingredient
+from users.models import User
+from shoppingcart.models import ShoppingCart
+
+class PantryView(APIView):
+    @action(detail=True, methods=['get'], url_path='pantry_return', url_name='pantry_return')
+    def get(self, request, pantry_id):
+        pantry = Pantry.objects.filter(id=pantry_id)
+        ingredients = pantry.values('ingredients')
+        ingredient_list = []
+        for key in ingredients:
+            ingredient_list.append(Ingredient.objects.filter(id=key['ingredients']).values('name').first())
+        data = {'ingredients': ingredient_list}
+        return JsonResponse(data, safe=False)
+
+
+class AddPantryIngredientsToCart(APIView):
+    @action(detail=True, methods=['post'])
+    def post(self,request, *args, **kwargs):
+        try:
+            pantry_id = request.data['pantry_id']
+            pantry = Pantry.objects.filter(id=pantry_id)
+            ingredient_list = pantry.values('ingredients')
+            user = User.objects.filter(username=request.user).first()
+            cart = ShoppingCart.objects.filter(user__id=user.id).first()
+            for key in ingredient_list:
+                single_item = Ingredient.objects.get(id=key['ingredients'])
+                cart.ingredients.add(single_item)
+            cart.save()
+            return JsonResponse({'success': True, 'message': ''})
+        except Exception as e:
+            return JsonResponse({'success':False, 'message': str(e)})
+            
