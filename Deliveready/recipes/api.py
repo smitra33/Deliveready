1c653464d829ff83a53ac0d9e4fd67de7d8e6d0d
smitra33 <smitra@ucalgary.ca>
2022-04-14 22:36:20 -0600

    API changes on recipes page and js logic tweaked

--- Deliveready/recipes/api.py
+++ Deliveready/recipes/api.py
@@ -7,15 +7,16 @@ from .models import Recipe
 from ingredients.models import Ingredient
 from users.models import User
 from shoppingcart.models import ShoppingCart
+from pantry.models import Pantry
 
-#this is just getting the queryset for one model
+
 class IngredientsAll(APIView):
     @action(detail=True, methods=['get'], url_path='ingredients_list', url_name='ingredients_list')
     def get(self, request, *args, **kwargs):
         ingredients = list(Ingredient.objects.all().values('name', 'quantity', 'quantity_unit', 'price'))
         return JsonResponse(ingredients, safe=False)
 
-#this is when a model has a many to many field with another model
+
 class IngredientsinRecipe(APIView):
     @action(detail=True, methods=['get'], url_path='ing_recipe_list', url_name='ing_recipe_list')
     def get(self, request, recipe_id):
@@ -26,7 +27,7 @@ class IngredientsinRecipe(APIView):
             ingredient_list.append(Ingredient.objects.filter(id=key['ingredients']).values('name').first())
         return JsonResponse(ingredient_list, safe=False)
 
-
+#retrieves recipe information
 class RecipeView(APIView):
     @action(detail=True, methods=['get'], url_path='recipe_return', url_name='recipe_return')
     def get(self, request, recipe_id):
@@ -40,7 +41,7 @@ class RecipeView(APIView):
         data = {'title': name['title'], 'ingredients': ingredient_list, 'instructions': instructions['instructions']}
         return JsonResponse(data, safe=False)
 
-
+#add recipe ingredients to shopping cart
 class AddRecipeIngredientsToCart(APIView):
     @action(detail=True, methods=['post'])
     def post(self, request, *args, **kwargs):
@@ -58,4 +59,39 @@ class AddRecipeIngredientsToCart(APIView):
         except Exception as e:
             return JsonResponse({'success': False, 'message': str(e)})
 
+#customer selection of ingredients after assessing duplicates added to shopping cart
+#input is a dictionary list of ingredients and quantity
+class AddSelectedIngredientsToCart(APIView):
+    @action(detail=True, methods=['post'])
+    def post(self, request, *args, **kwargs):
+        try:
 
+            return JsonResponse({'success': True, 'message': ''})
+        except Exception as e:
+            return JsonResponse({'success': False, 'message': str(e)})
+
+#retrieves ingredient quantity information in shopping cart
+#output is dictionary list of ingredients, quantity, price
+class CheckSelectedIngredientsToCart(APIView):
+    @action(detail=True, methods=['post'])
+    def post(self, request, *args, **kwargs):
+        return JsonResponse(True, safe=False)
+
+class CheckIngredientsPantry(APIView):
+    @action(detail=True, methods=['get'], url_path='check_ingredients', url_name='check_ingredients')
+    def get(self, request):
+        try:
+            user = User.objects.filter(username=request.user).first()
+            cart_list = ShoppingCart.objects.filter(user__id=user.id).values('ingredients')
+            pantry_list = Pantry.objects.filter(user__id=user.id).values('ingredients')
+            duplicates = [elem for elem in cart_list if elem in pantry_list]
+            non_duplicates = [elem for elem in cart_list if elem not in pantry_list]
+            dup_list = []
+            non_dup_list = []
+            for dup in duplicates:
+                dup_list.append(Ingredient.objects.filter(id=dup['ingredients']).values('name').first())
+            for ndup in non_duplicates:
+                non_dup_list.append(Ingredient.objects.filter(id=ndup['ingredients']).values('name').first())
+            return JsonResponse({'success': True, 'duplicates': dup_list, 'non-dupes': non_dup_list})
+        except Exception as e:
+            return JsonResponse({'success': False, 'message': str(e)})
