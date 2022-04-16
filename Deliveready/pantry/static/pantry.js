686570e4965d28a3fee2e1cd64589c45cb3b37dc
dohkhanh <do.h.khanh@gmail.com>
2022-04-15 20:57:11 -0600

    changed header colours

--- Deliveready/pantry/static/pantry.js
+++ Deliveready/pantry/static/pantry.js
@@ -108,7 +108,7 @@ function addToPantryFromButton() {
     addToPantry(ingredient);
 }
 
-addBtn.addEventListener('click', addToPantryFromButton); 
+addBtn.addEventListener('click', addToPantryFromButton);
 
 async function addToPantryDatabase(targetIng) {
     targetIng = targetIng.charAt(0).toUpperCase() + targetIng.slice(1);
